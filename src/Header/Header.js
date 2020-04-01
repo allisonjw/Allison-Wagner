cca79e3cfd4a08528620a6697f83b26e601db682
Allison Wagner <allisonjw01@gmail.com>
2020-04-01 15:04:12 -0600

    Adds skeleton files for projects and header

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+export const Header = () => {
+    return (
+        <section className="header_section">
+            <h3 className="header_h3">
+                Allison Wagner || Software Engineer 
+            </h3>
+        </section>
+    )
+}
