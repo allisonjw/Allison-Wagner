995e0224c1a0b7c12f36657f8ca20cbb58f342f3
Allison Wagner <allisonjw01@gmail.com>
2020-04-01 15:02:04 -0600

    Sets up skeleton for contact and about file

---
+++ src/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.scss";
+
+export const About = () => {
+    return (
+        <section className="about_section">
+            <h3 className="about_h3">
+                About Me
+            </h3>
+        </section>
+    )
+}
---
+++ src/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.scss";
+
+export const Contact = () => {
+    return (
+        <section className="contact_section">
+            <h3 className="contact_h3">
+                Contact Me
+            </h3>
+        </section>
+    )
+}
