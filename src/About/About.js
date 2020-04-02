40e593753c3e4ccf5396be6f343cbaf984fceb0a
Allison Wagner <allisonjw01@gmail.com>
2020-04-02 14:54:22 -0600

    Adds additional elements to style about with side by side information and additional styling

--- src/About/About.js
+++ src/About/About.js
@@ -16,18 +16,19 @@ export const About = () => {
       };
     
     return (
-        <section className="about_section">
-            <h2 className="about_h2">
-                About Me
-            </h2>
-            <h3 className="about_h3">{name}</h3>
-            <h3 className="about_h3">{title}</h3>
-            <h3 className="about_h3">{description}</h3>
-            <h3 className="about_h3">{image}</h3>
+        <div className="about_container">
+            <section className="about_section">
+                <h2 className="about_h2">
+                    About Me
+                </h2>
+                <h3 className="about_h3">{name}  ||  {title}</h3>
+                <h3 className="about_h3">{description}</h3>
+                <h3 className="about_h3">{image}</h3>
+            </section>
             <aside className="aside_skills">
-                <h3 className="about_h3">Skills:</h3>
-                {skillsList()}
+                <h3 className="about_skills">Skills:</h3>
+                <h4>{skillsList()}</h4>
             </aside>
-        </section>
+        </div>
     )
 }
