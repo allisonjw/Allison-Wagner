3b04a388af336f8eb1f1d4a1063bbfb86b3e8d67
Allison Wagner <allisonjw01@gmail.com>
2020-04-02 10:23:44 -0600

    Imports allInfo and starts adding about data

--- src/About/About.js
+++ src/About/About.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./About.scss";
+import { contactInfo } from "../allInfo/allInfo";
+
+const { name, title, description, image, skills } = contactInfo;
 
 export const About = () => {
+    const skillsList = () => {
+        return skills.map(skill => {
+          return (
+            <p className="skill" key={skill}>
+              {skill}
+            </p>
+          );
+        });
+      };
+    
     return (
         <section className="about_section">
-            <h3 className="about_h3">
+            <h2 className="about_h2">
                 About Me
-            </h3>
+            </h2>
+            <h3 className="about_h3">{name}</h3>
+            <h3 className="about_h3">{title}</h3>
+            <h3 className="about_h3">{description}</h3>
+            <h3 className="about_h3">{image}</h3>
+            <aside className="aside_skills">{skillsList()}</aside>
         </section>
     )
 }
