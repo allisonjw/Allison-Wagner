7fe46fa78258a84ffa1b4d8f294b2517a1be4014
Allison Wagner <allisonjw01@gmail.com>
2020-04-02 16:02:36 -0600

    Adding classNames to contact info and react icons with additional styling

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./Contact.scss";
+import { contactInfo } from "../allInfo/allInfo";
+import { AiOutlineMail } from "react-icons/ai";
+import { TiDocumentText } from "react-icons/ti";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const { email, gitHub, linkedIn, resume } = contactInfo;
 
 export const Contact = () => {
     return (
         <section className="contact_section">
-            <h3 className="contact_h3">
+            <h2 className="contact_h2">
                 Contact Me
-            </h3>
+            </h2>
+                <h3 className="contact_h3">Email: 
+                <a href={email}>Email</a>
+                <span className="contact_icons"><AiOutlineMail /></span>
+                </h3>
+                <h3 className="contact_h3">GitHub: {gitHub}
+                <FaGithub />
+                </h3>
+                <h3 className="contact_h3">Resume: {linkedIn}
+                <TiDocumentText />
+                </h3>
+                <h3 className="contact_h3">LinkedIn: {resume}
+                <FaLinkedinIn />
+                </h3>
         </section>
     )
 }
