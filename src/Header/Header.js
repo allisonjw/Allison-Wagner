26dafb9b0296640403ed712a26bf71cb5634c612
Allison Wagner <allisonjw01@gmail.com>
2020-04-02 14:55:16 -0600

    Changes background color of h1 and size of header with additional styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Header.scss";
+import { FaChevronDown } from 'react-icons/fa';
 
 export const Header = () => {
     return (
         <header>
-            <h3 className="header_h3">
+            <h1>
                 Allison Wagner || Software Engineer 
-            </h3>
-            <i class="ion-chevron-down">scroll down</i>
+            </h1>
+            <div class="ion-chevron-down">
+            <h3>scroll down</h3>
+            <FaChevronDown />
+            </div>
         </header>
     )
 }
