4a910679deb0a99763b4cd0569e1084833ebbd5e
Allison Wagner <allisonjw01@gmail.com>
2020-04-02 18:50:18 -0600

    Displays projects and starts styles

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "./Projects.scss";
+import { FaGithub } from "react-icons/fa";
+import { MdLaunch } from "react-icons/md";
 
-export const Projects = () => {
+export const Projects = ({ project }) => {
+    const { title, description, gitHub, live, image } = project;
+
     return (
         <section className="projects_section">
-            <h3 className="projects_h3">
-                My Projects
-            </h3>
-        </section>
+      <article className="description_container">
+        <h3 className="title">
+          {title}
+        </h3>
+        <p className="description">
+          {description}
+        </p>
+        <aside className="icon_container">
+          {gitHub && (
+            <a href={gitHub} target="_blank">
+              <FaGithub />
+            </a>
+          )}
+          {live && (
+            <a href={live} target="_blank">
+              <MdLaunch />
+            </a>
+          )}
+        </aside>
+      </article>
+      <article className="image_container">
+        <img className="image" src={image} alt="Project" />
+      </article>
+    </section>
     )
 }
