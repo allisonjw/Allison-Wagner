f75134fa1b1a5dac0d1c6e84bd2959921658613e
Allison Wagner <allisonjw01@hotmail.com>
2020-08-19 12:59:20 -0600

    Fixed imports in footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,9 +1,9 @@
-import React from "./node_modules/react";
+import React from "react";
 import "./Footer.scss";
 import { contactInfo } from "../allInfo/allInfo";
-import { AiOutlineMail, AiOutlineCopyright } from "./node_modules/react-icons/ai";
-import { TiDocumentText } from "./node_modules/react-icons/ti";
-import { FaGithub, FaLinkedinIn, FaRegCopyright } from "./node_modules/react-icons/fa";
+import { AiOutlineMail, AiOutlineCopyright } from "react-icons/ai";
+import { TiDocumentText } from "react-icons/ti";
+import { FaGithub, FaLinkedinIn, FaRegCopyright } from "react-icons/fa";
 
 const { email, gitHub, linkedIn, resume } = contactInfo;
 
