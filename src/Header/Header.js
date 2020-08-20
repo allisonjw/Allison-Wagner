d76fadfa208ef7d67126efb1c3810e794f28b669
Allison Wagner <allisonjw01@hotmail.com>
2020-08-20 13:57:13 -0600

    Adds navbar nested in header and adjust styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.scss";
+import { Navbar } from "../Navbar/Navbar";
 import { FaChevronDown } from 'react-icons/fa';
 
 export const Header = () => {
@@ -8,6 +9,7 @@ export const Header = () => {
             <h1>
                 Allison Wagner || Software Engineer 
             </h1>
+            <Navbar />
             <div className="scroll_down">
             <h3>scroll down</h3>
             <FaChevronDown className="header_arrow"/>
